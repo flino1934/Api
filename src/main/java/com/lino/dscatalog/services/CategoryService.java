99a1a453b163f725669f25ba97b12422eaf5a3c2
Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>
2024-08-12 18:05:39 -0300

    Implements find by id of product
    Signed-off-by: Felipe Alves Lino dos Santos <felipe.alves@nava.com.br>

--- src/main/java/com/lino/dscatalog/services/CategoryService.java
+++ src/main/java/com/lino/dscatalog/services/CategoryService.java
@@ -5,8 +5,10 @@ import com.lino.dscatalog.entities.Category;
 import com.lino.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,6 +17,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
 
         List<Category> list = repository.findAll();
@@ -24,4 +27,11 @@ public class CategoryService {
 
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
+    }
 }
