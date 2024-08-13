2da7c7f1dd0d893d0094749fccad4828c1fcf668
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-13 19:10:01 -0300

    Data auditing
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (name) VALUES ('Livros');
-INSERT INTO tb_category (name) VALUES ('Eletrônicos');
-INSERT INTO tb_category (name) VALUES ('Computadores');
+INSERT INTO tb_category (name, created_At) VALUES ('Livros', now());
+INSERT INTO tb_category (name, created_At) VALUES ('Eletrônicos', now());
+INSERT INTO tb_category (name, created_At) VALUES ('Computadores', now());
