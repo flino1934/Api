fdf55d24558d1b657fe0aab73c5d12fc5f7018cb
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-14 17:39:17 -0300

    Creat entity, dto and data seeding
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

---
+++ src/main/java/com/lino/dscatalog/dto/ClientDTO.java
@@ -0,0 +1,74 @@
+package com.lino.dscatalog.dto;
+
+import com.lino.dscatalog.entities.Client;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class ClientDTO implements Serializable {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Instant birthDate;
+    private Integer children;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(Long id, String name, String cpf, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
