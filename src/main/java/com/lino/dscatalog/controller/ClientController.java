1cbe281a7054aea4da96b18f8b1610ef1c00eb9c
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-14 19:42:55 -0300

    Finish crud Client
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/main/java/com/lino/dscatalog/controller/ClientController.java
+++ src/main/java/com/lino/dscatalog/controller/ClientController.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/clients")
@@ -30,6 +30,33 @@ public class ClientController {
 
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+
+        dto = service.update(id,dto);
+        return ResponseEntity.ok().body(dto);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ClientDTO> delete(@PathVariable Long id ){
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
 
     }
 
--- src/main/java/com/lino/dscatalog/services/ClientService.java
+++ src/main/java/com/lino/dscatalog/services/ClientService.java
@@ -1,17 +1,18 @@
 package com.lino.dscatalog.services;
 
-import com.lino.dscatalog.dto.CategoryDTO;
 import com.lino.dscatalog.dto.ClientDTO;
-import com.lino.dscatalog.entities.Category;
 import com.lino.dscatalog.entities.Client;
 import com.lino.dscatalog.repositories.ClientRepository;
+import com.lino.dscatalog.services.services.exceptions.DataBaseExceptions;
 import com.lino.dscatalog.services.services.exceptions.ResourceNotFoundExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -20,13 +21,14 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(Pageable pageable) {
 
         Page<Client> list = repository.findAll(pageable);
         return list.map(x -> new ClientDTO(x));
     }
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
 
         Optional<Client> obj = repository.findById(id);
@@ -35,5 +37,56 @@ public class ClientService {
 
         return dto;
 
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+
+        Client client = new Client();
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setChildren(dto.getChildren());
+        client.setBirthDate(dto.getBirthDate());
+
+        repository.save(client);
+
+        return new ClientDTO(client);
+
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+
+        try {
+            Client entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity.setChildren(dto.getChildren());
+
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+
+            throw new ResourceNotFoundExceptions("Id not found " + id);
+
+        }
+
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        try {
+
+            Client clientDTO = repository.getOne(id);
+            repository.delete(clientDTO);
+
+        } catch (EntityNotFoundException e) {
+
+            throw new ResourceNotFoundExceptions("Id not found " + id);
+
+        }catch (DataIntegrityViolationException e){
+
+            throw new DataBaseExceptions("Database Violation");
+
+        }
     }
 }
