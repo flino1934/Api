1004a1ec62d126bb53979bf8023ffe8f4078c0cf
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-21 15:56:46 -0300

    implements test delete layer controller
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/controller/CategoryControllerTest.java
+++ src/test/java/com/lino/dscatalog/controller/CategoryControllerTest.java
@@ -8,6 +8,7 @@ import com.lino.dscatalog.factory.CategoryFactory;
 import com.lino.dscatalog.factory.ProductFactory;
 import com.lino.dscatalog.services.CategoryService;
 import com.lino.dscatalog.services.ProductService;
+import com.lino.dscatalog.services.exceptions.DataBaseExceptions;
 import com.lino.dscatalog.services.exceptions.ResourceNotFoundExceptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -52,7 +54,7 @@ public class CategoryControllerTest {
         //Estara fazendo o Arrange
         existingId = 1L;
         nonExistingId = 1000L;
-        dependentId = 23L;
+        dependentId = 3L;
         categoryDTO = CategoryFactory.createCategoryDTORepository();
         page = new PageImpl<>(List.of(categoryDTO));
 
@@ -69,6 +71,15 @@ public class CategoryControllerTest {
 
         //Vai simular o insert
         Mockito.when(service.insert(ArgumentMatchers.any())).thenReturn(categoryDTO);
+
+        //Vai simular o delete quando o id existir
+        Mockito.doNothing().when(service).delete(existingId);
+
+        //Vai simular o delete quando o id não existir e vai lançar ResourceNotFoundExceptions
+        Mockito.doThrow(ResourceNotFoundExceptions.class).when(service).delete(nonExistingId);
+
+        //Vai simular o delete quando o id for dependete e vai lançar DataBaseExceptions
+        Mockito.doThrow(DataBaseExceptions.class).when(service).delete(dependentId);
 
     }
 
@@ -118,6 +129,38 @@ public class CategoryControllerTest {
                         .accept(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isCreated());
+
+    }
+
+    @Test
+    public void testDeleteWhenIdExist() throws Exception{
+
+        ResultActions result =
+                mockMvc.perform(delete("/api/categories/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void testDeleteWhenIdDoesNotExistShouldReturnResourceNotFoundExceptions() throws Exception{
+
+        ResultActions result =
+                mockMvc.perform(delete("/api/categories/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+
+    }
+    @Test
+    public void testDeleteWhenIdDependentShouldReturnException() throws Exception{
+
+        ResultActions result =
+                mockMvc.perform(delete("/api/categories/{id}", dependentId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
 
     }
 
