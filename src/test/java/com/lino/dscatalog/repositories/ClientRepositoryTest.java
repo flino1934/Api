7fa7ddd5b6f4c4f8c4a69a75c64846f6f12c18fe
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-22 11:30:30 -0300

    implements unit test find by id when id exist and does nothing exist
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/repositories/ClientRepositoryTest.java
+++ src/test/java/com/lino/dscatalog/repositories/ClientRepositoryTest.java
@@ -4,6 +4,7 @@ import com.lino.dscatalog.entities.Category;
 import com.lino.dscatalog.entities.Client;
 import com.lino.dscatalog.factory.CategoryFactory;
 import com.lino.dscatalog.factory.ClientFactory;
+import com.lino.dscatalog.services.exceptions.ResourceNotFoundExceptions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 @DataJpaTest
 public class ClientRepositoryTest {
@@ -48,6 +51,28 @@ public class ClientRepositoryTest {
 
         //Assert
         Assertions.assertNotNull(result);
+
+    }
+
+    @Test
+    public void testFindByIDShouldReturnClientWhenIdExist(){
+
+        //Arrange
+
+        //Act
+        Optional<Client> result = repository.findById(existingId);
+
+        //Assertion
+        Assertions.assertTrue(result.isPresent());
+
+    }
+
+    @Test
+    public void testFindByIdWhenIdDoesNothingExist(){
+
+        Optional<Client> result = repository.findById(nonExistingId);
+
+        Assertions.assertTrue(result.isEmpty());
 
     }
 
