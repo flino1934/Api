3052f52cc6b09ca0aabf4c3e4472f47180f203a2
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-24 11:13:42 -0300

    implements find by id users
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/main/java/com/lino/dscatalog/controller/UserController.java
+++ src/main/java/com/lino/dscatalog/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.lino.dscatalog.controller;
 
-import com.lino.dscatalog.dto.CategoryDTO;
 import com.lino.dscatalog.dto.UserDTO;
 import com.lino.dscatalog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +19,18 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable){
+    public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable) {
 
         Page<UserDTO> list = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
 
     }
 
--- src/main/java/com/lino/dscatalog/services/UserService.java
+++ src/main/java/com/lino/dscatalog/services/UserService.java
@@ -1,15 +1,16 @@
 package com.lino.dscatalog.services;
 
-import com.lino.dscatalog.dto.CategoryDTO;
 import com.lino.dscatalog.dto.UserDTO;
 import com.lino.dscatalog.entities.User;
 import com.lino.dscatalog.repositories.UserRepository;
+import com.lino.dscatalog.services.exceptions.ResourceNotFoundExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,6 +23,15 @@ public class UserService {
 
         Page<User> list = repository.findAll(pageable);
         return list.map(x -> new UserDTO(x));
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(()-> new ResourceNotFoundExceptions("User not found!!"));
+        return new UserDTO(entity);
 
     }
 }
