5b963e89324493a6c42d397a4b7e8c9aca6dcefa
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-26 15:56:32 -0300

    Implements spring security
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/main/java/com/lino/dscatalog/services/UserService.java
+++ src/main/java/com/lino/dscatalog/services/UserService.java
@@ -10,11 +10,16 @@ import com.lino.dscatalog.repositories.RoleRepository;
 import com.lino.dscatalog.repositories.UserRepository;
 import com.lino.dscatalog.services.exceptions.DataBaseExceptions;
 import com.lino.dscatalog.services.exceptions.ResourceNotFoundExceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +27,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -96,17 +103,29 @@ public class UserService {
 
     public void delete(Long id) {
 
-        try{
+        try {
             repository.deleteById(id);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
 
             throw new ResourceNotFoundExceptions("Id not found!!!");
 
-        }catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
 
             throw new DataBaseExceptions("Databse violation!!");
 
         }
 
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {//Ele vai retornar o userDetail correspondente a quem pedimos fazendo toda verificação
+
+        User user = repository.findByEmail(email);
+        if (user == null) {
+            logger.error("User not found " + email);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("User found " + user);
+        return user;
     }
 }
