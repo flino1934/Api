4550bfa6e4ab162b94781a2332a3a5ad0b955864
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-28 16:01:07 -0300

    implements test find all paged repository
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/repositories/UserRepositoryTest.java
+++ src/test/java/com/lino/dscatalog/repositories/UserRepositoryTest.java
@@ -4,9 +4,14 @@ import com.lino.dscatalog.entities.Client;
 import com.lino.dscatalog.entities.User;
 import com.lino.dscatalog.factory.ClientFactory;
 import com.lino.dscatalog.factory.UserFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest//Usado para testar a camada de repository
 public class UserRepositoryTest {
@@ -26,6 +31,20 @@ public class UserRepositoryTest {
         existingId = 1L;
         nonExistingId = 1000L;
         user = UserFactory.createUser();
+    }
+
+    @Test
+    public void testFindAllPaged() {
+
+        //Arrange
+        Pageable pageable = PageRequest.of(0, 10);
+
+        //Act
+        Page<User> result = repository.findAll(pageable);
+
+        //Assert
+        Assertions.assertNotNull(result);
+
     }
 
 }
