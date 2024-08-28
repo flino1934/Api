0e6a494421d92b5da71e7aaaaa1eaab98d5a48a1
FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>
2024-08-28 16:17:05 -0300

    Create test layer repository find by usuario***
    Signed-off-by: FELIPE ALVES LINO DOS SANTOS <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/repositories/UserRepositoryTest.java
+++ src/test/java/com/lino/dscatalog/repositories/UserRepositoryTest.java
@@ -13,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 @DataJpaTest//Usado para testar a camada de repository
 public class UserRepositoryTest {
 
@@ -44,6 +46,41 @@ public class UserRepositoryTest {
 
         //Assert
         Assertions.assertNotNull(result);
+
+    }
+
+    @Test
+    public void testFindByIdWhenIdExist(){
+
+        //Arrange and  Act
+        Optional<User> obj = repository.findById(existingId);
+
+        //Assert
+        Assertions.assertTrue(obj.isPresent());
+
+    }
+
+    @Test
+    public void testFindByIdWhenIdDoesnothingExist(){
+
+        //Arrange and  Act
+        Optional<User> obj = repository.findById(nonExistingId);
+
+        //Assert
+        Assertions.assertTrue(obj.isEmpty());
+    }
+
+    @Test
+    public void testFindByEmailReturnUser(){
+
+        //Arrange and act
+
+        User find = repository.findByEmail(user.getEmail());
+
+        //Assertions
+        Assertions.assertEquals("mica@gmail.com", find.getEmail());
+        Assertions.assertEquals("Michaelly Monique", find.getFirstName());
+        Assertions.assertEquals("Oliveira Di Pardo",user.getLastName());
 
     }
 
